a73a60dec87554f2d5a9feee056840cae306e303
mini.trivedi <mini.trivedi3@gmail.com>
2023-10-02 02:54:26 +0530

    adding new changes

--- main.tf
+++ main.tf
@@ -1,7 +1,26 @@
-resource "aws_instance" "demo" {
- ami = “ami-00831fc7c1e3ddc60”
- instance_type = “t2.micro”
+data "aws_region" "current" {}
 
- tags = {
-   name = "Demo System"
- }
+resource "aws_vpc_ipam" "test" {
+  operating_regions {
+    region_name = data.aws_region.current.name
+  }
+}
+
+resource "aws_vpc_ipam_pool" "test" {
+  address_family = "ipv4"
+  ipam_scope_id  = aws_vpc_ipam.test.private_default_scope_id
+  locale         = data.aws_region.current.name
+}
+
+resource "aws_vpc_ipam_pool_cidr" "test" {
+  ipam_pool_id = aws_vpc_ipam_pool.test.id
+  cidr         = "172.20.0.0/16"
+}
+
+resource "aws_vpc" "test" {
+  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.test.id
+  ipv4_netmask_length = 28
+  depends_on = [
+    aws_vpc_ipam_pool_cidr.test
+  ]
+}
